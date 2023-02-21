380e2477fa91f94857b52fbe76a8c34867d23ea7
Folderas <82723806+marcinwolder@users.noreply.github.com>
2023-02-21 23:39:33 +0100

    menu sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,51 @@
 import Navbar from './components/Navbar';
 import { createRef } from 'react';
 
-import { useEffect } from 'react'
-import { themeChange } from 'theme-change'
+import { useEffect } from 'react';
+import { themeChange } from 'theme-change';
 
 function App() {
-  useEffect(() => {
-    themeChange(false)
-  }, [])
-  const ref = createRef<HTMLInputElement>()
+	useEffect(() => {
+		themeChange(false);
+	}, []);
+	const ref = createRef<HTMLInputElement>();
 	return (
 		<div className='drawer'>
-			<input ref={ref} id='my-drawer' type='checkbox' className='drawer-toggle' />
+			<input
+				ref={ref}
+				id='my-drawer'
+				type='checkbox'
+				className='drawer-toggle'
+			/>
 			<div className='drawer-content'>
 				<Navbar menuRef={ref} />
 			</div>
 			<div className='drawer-side'>
 				<label htmlFor='my-drawer' className='drawer-overlay'></label>
 				<ul className='menu p-4 w-80 bg-base-100 text-base-content'>
-					<li>
-						<a>Sidebar Item 1</a>
+					<li className='menu-title'>
+						<span>FORMUŁA 2023</span>
 					</li>
 					<li>
-						<a>Sidebar Item 2</a>
+						<a>Arkusz przykładowy</a>
+					</li>
+					<li className='menu-title'>
+						<span>FORMUŁA 2015</span>
+					</li>
+					<li>
+						<a>Matura 2022 - Maj</a>
+					</li>
+					<li>
+						<a>Matura 2022 - Czerwiec</a>
+					</li>
+					<li className='menu-title'>
+						<span>STARA FORMUŁA</span>
+					</li>
+					<li>
+						<a>Item 1</a>
+					</li>
+					<li>
+						<a>Item 2</a>
 					</li>
 				</ul>
 			</div>
