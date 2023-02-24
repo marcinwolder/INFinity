8278005b814711e82cc584cf55e4875067bd702d
Folderas <82723806+marcinwolder@users.noreply.github.com>
2023-02-25 00:46:50 +0100

    basic matura exam site template

--- src/routes/formula-2015/2022/maj.tsx
+++ src/routes/formula-2015/2022/maj.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Maj2022 = () => {
-	return <div>Maj</div>;
+	const [tab, setTab] = useState(1);
+	return (
+		<div className='px-6'>
+			<div className='breadcrumbs'>
+				<ul>
+					<li>
+						<Link to={'/'}>Strona Główna</Link>
+					</li>
+					<li>
+						<Link to={'/formula-2015'}>Formuła 2015</Link>
+					</li>
+					<li>Matura - Maj 2022</li>
+				</ul>
+			</div>
+			<div className='tabs'>
+				<a
+					onClick={() => setTab(1)}
+					className={`tab tab-bordered ${tab === 1 && 'tab-active'}`}>
+					Część 1
+				</a>
+				<a
+					onClick={() => setTab(2)}
+					className={`tab tab-bordered ${tab === 2 && 'tab-active'}`}>
+					Część 2
+				</a>
+			</div>
+			<div>{tab === 1 ? 'Tab1' : 'Tab2'}</div>
+		</div>
+	);
 };
 
 export default Maj2022;
