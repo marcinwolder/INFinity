c39f355e21fd8ffd84f97c2f5f05689853f93b34
Folderas <82723806+marcinwolder@users.noreply.github.com>
2023-02-24 17:14:30 +0100

    tiny YearDropdown remake

--- src/components/YearDropdown.tsx
+++ src/components/YearDropdown.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
 
 interface props {
-	setOpen: React.Dispatch<React.SetStateAction<string>>;
-	open: string;
 	year: string;
 }
 
-const YearDropdown: React.FC<React.PropsWithChildren<props>> = ({
-	setOpen,
-	year,
-	open,
-	children,
-}) => {
-	if (open == year) {
+const useYearDropdown = (
+	open: string,
+	setOpen: React.Dispatch<React.SetStateAction<string>>
+): React.FC<React.PropsWithChildren<props>> => {
+	const YearDropdown: React.FC<React.PropsWithChildren<props>> = ({
+		year,
+		children,
+	}) => {
+		if (open == year) {
+			return (
+				<div
+					tabIndex={0}
+					data-year={year}
+					className='collapse collapse-open collapse-arrow border border-base-300 bg-base-200 rounded-box w-80'
+					onClick={() => setOpen(year)}>
+					<div className='collapse-title text-xl font-medium'>{year}</div>
+					<div className='collapse-content'>
+						<ul className='menu menu-compact'>{children}</ul>
+					</div>
+				</div>
+			);
+		}
 		return (
 			<div
 				tabIndex={0}
 				data-year={year}
-				className='collapse collapse-open collapse-arrow border border-base-300 bg-base-200 rounded-box w-80'
+				className='collapse collapse-arrow border border-base-300 bg-base-200 rounded-box w-80'
 				onClick={() => setOpen(year)}>
 				<div className='collapse-title text-xl font-medium'>{year}</div>
 				<div className='collapse-content'>
@@ -25,19 +38,8 @@ const YearDropdown: React.FC<React.PropsWithChildren<props>> = ({
 				</div>
 			</div>
 		);
-	}
-	return (
-		<div
-			tabIndex={0}
-			data-year={year}
-			className='collapse collapse-arrow border border-base-300 bg-base-200 rounded-box w-80'
-			onClick={() => setOpen(year)}>
-			<div className='collapse-title text-xl font-medium'>{year}</div>
-			<div className='collapse-content'>
-				<ul className='menu menu-compact'>{children}</ul>
-			</div>
-		</div>
-	);
+	};
+	return YearDropdown;
 };
 
-export default YearDropdown;
+export default useYearDropdown;
--- src/routes/Formula2015.tsx
+++ src/routes/Formula2015.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 import { usePath } from '../context/menuContext';
-import YearDropdown from '../components/YearDropdown';
+import useYearDropdown from '../components/YearDropdown';
 
 const mapOpened = (
 	elements: React.ReactElement<React.PropsWithChildren<{}>>,
@@ -31,13 +31,16 @@ const mapOpened = (
 
 const Main = () => {
 	const url = usePath('/formula-2015');
+
 	const [open, setOpen] = useState('2022');
+	const YearDropdown = useYearDropdown(open, setOpen);
+
 	return (
 		<div className='artboard gap-4 flex flex-col items-center relative'>
 			<h1 className='text-2xl small-caps self-start p-2 pr-4 rounded-tr-xl rounded-br-md bg-secondary-focus text-secondary-content'>
 				Formuła 2015
 			</h1>
-			<YearDropdown year='2022' open={open} setOpen={setOpen}>
+			<YearDropdown year='2022'>
 				<li>
 					<Link to={url + '/2022/maj'} className='text-lg'>
 						Maj
@@ -55,7 +58,7 @@ const Main = () => {
 					</a>
 				</li>
 			</YearDropdown>
-			<YearDropdown year='2021' open={open} setOpen={setOpen}>
+			<YearDropdown year='2021'>
 				<li>
 					<a className='text-lg'>
 						Marzec
