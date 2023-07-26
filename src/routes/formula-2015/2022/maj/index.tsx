290d822296793ab02dce764b8f5da2c299a279cb
Folderas <82723806+marcinwolder@users.noreply.github.com>
2023-07-26 22:29:04 +0200

    dynamic import fixed

--- src/routes/formula-2015/2022/maj/index.tsx
+++ src/routes/formula-2015/2022/maj/index.tsx
@@ -1,26 +1,47 @@
+import { ComponentType, lazy } from 'react';
+
 import { useMaturaPath, usePath } from '../../../../redux/slices/pathSlice';
 
 import PartSwitch from '../../../../components/PartSwitch';
 import TabSwitch from '../../../../components/TabSwitch';
 
-import Analiza from './Analiza';
-import Test from './Test';
-import Python from './Python';
-import Excel from './Excel';
-import Access from './Access';
-import { lazy } from 'react';
+import Error from '../../../../components/Error';
+
+const modules = import.meta.glob('/src/routes/*/*/*/*.tsx') as Record<
+	string,
+	() => Promise<{ default: ComponentType }>
+>;
+
+const handleImport = (url: string) =>
+	lazy(() => {
+		if (modules[url]) return modules[url]();
+		else
+			return Promise.reject().catch(() => ({
+				default: Error,
+			}));
+	});
 
 const Maj2022 = () => {
 	usePath('/2022/maj');
 	const maturaPath = useMaturaPath();
 
-	const Algorytm = lazy(() =>
-		import(
-			`/* @vite-ignore */ /src/routes/${maturaPath.formula}/${maturaPath.date}/Algorytm`
-		).catch((err) => {
-			console.log(err);
-			return import('../../../../components/Error.tsx');
-		})
+	const Algorytm = handleImport(
+		`/src/routes/${maturaPath.formula}/${maturaPath.date}/Algorytm.tsx`
+	);
+	const Analiza = handleImport(
+		`/src/routes/${maturaPath.formula}/${maturaPath.date}/Analiza.tsx`
+	);
+	const Test = handleImport(
+		`/src/routes/${maturaPath.formula}/${maturaPath.date}/Test.tsx`
+	);
+	const Python = handleImport(
+		`/src/routes/${maturaPath.formula}/${maturaPath.date}/Python.tsx`
+	);
+	const Excel = handleImport(
+		`/src/routes/${maturaPath.formula}/${maturaPath.date}/Excel.tsx`
+	);
+	const Access = handleImport(
+		`/src/routes/${maturaPath.formula}/${maturaPath.date}/Access.tsx`
 	);
 
 	return (
