aaec5da9fb2ac888d8af438ad65c729904df5b0a
Folderas <82723806+marcinwolder@users.noreply.github.com>
2023-08-25 11:08:28 +0200

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import { qrcode } from 'vite-plugin-qrcode';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), basicSsl()],
+	plugins: [react(), basicSsl(), qrcode()],
 });
