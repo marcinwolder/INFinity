2e500b04bb7a5635775a84a6a399f66880e4c6b4
Folderas <82723806+marcinwolder@users.noreply.github.com>
2023-08-31 19:52:03 +0200

    responsive design of kursy page

--- src/routes/Kursy.tsx
+++ src/routes/Kursy.tsx
@@ -8,18 +8,20 @@ import kursAlgo from './../img/kursy/kurs-algo.png';
 const Kursy: React.FC = () => {
 	return (
 		<>
-			<div className='flex gap-4 items-end'>
-				<div className='border border-base-300 bg-base-200 p-6 px-8 rounded-sm'>
+			<div className='px-4 flex flex-col lg:flex-row gap-4 items-center lg:items-end'>
+				<div className='border border-base-300 bg-base-200 p-6 px-8 rounded-sm w-full lg:w-fit shrink-0'>
 					<code className='text-primary uppercase font-black text-xl tracking-widest'>
 						pojedynczy kurs
 					</code>
-					<p className='text-sm'>Idealny aby uzupełnić braki.</p>
+					<div className='text-sm h-10'>Idealny aby uzupełnić braki.</div>
 					<div className='divider' />
 					<span className='text-4xl font-bold'>80</span>
-					<span className='text-2xl'>,00zł</span>
-					<span className='text-xs ml-4'>/ jednorazowa płatność</span>
+					<span className='text-2xl'>,00 PLN</span>
+					<div className='text-xs ml-4'>/ jednorazowa płatność</div>
 					<div className='divider' />
-					<p className='text-sm mb-2'>Zawiera jeden z dostępnych kursów:</p>
+					<div className='text-sm mb-2 h-10'>
+						Zawiera jeden z dostępnych kursów:
+					</div>
 					<div className='flex items-center gap-1'>
 						<img className='h-14' src={kursPython} alt='kursPython' />
 						<div>/</div>
@@ -29,49 +31,63 @@ const Kursy: React.FC = () => {
 						<div>/</div>
 						<img className='h-14' src={kursAlgo} alt='kursAlgo' />
 					</div>
+					<button className='block btn-success hover:btn-neutral btn btn-sm w-24 ml-auto mt-16'>
+						Kup
+					</button>
 				</div>
-				<div className='relative rounded-sm outline outline-success outline-4'>
+				<div className='relative rounded-sm outline outline-success outline-4 w-[calc(100%-theme(spacing.2))] lg:w-fit shrink-0'>
 					<div className='text-center text-sm font-semibold py-1 bg-success text-success-content'>
 						Najczęściej Kupowany
 					</div>
 					<div className='bg-base-200 p-6 px-8 rounded-sm outline-success'>
 						<code className='text-primary uppercase font-black text-xl tracking-widest'>
-							Pełen zestaw
+							Pełen zestaw kursów
 						</code>
-						<p className='text-sm'>Pełne przygotowanie do matury.</p>
+						<div className='text-sm h-10'>Pełne przygotowanie do matury.</div>
 						<div className='divider' />
 						<span className='text-4xl font-bold'>200</span>
-						<span className='text-2xl'>,00zł</span>
-						<span className='text-xs ml-4'>/ jednorazowa płatność</span>
+						<span className='text-2xl'>,00 PLN</span>
+						<div className='text-xs ml-4'>/ jednorazowa płatność</div>
 						<div className='divider' />
-						<p className='text-sm mb-2'>Zawiera wszystkie dostępne kursy:</p>
+						<div className='text-sm mb-2 h-10'>
+							Zawiera wszystkie dostępne kursy:
+						</div>
 						<div className='flex items-center [&>*]:-mr-6'>
 							<img className='h-14' src={kursPython} alt='kursPython' />
 							<img className='h-14' src={kursExcel} alt='kursExcel' />
 							<img className='h-14' src={kursAccess} alt='kursAccess' />
 							<img className='h-14' src={kursAlgo} alt='kursAlgo' />
 						</div>
+						<button className='block btn-success hover:btn-neutral btn btn-sm w-24 ml-auto mt-16'>
+							Kup
+						</button>
 					</div>
 					<div className='absolute -z-10 inset-0 bg-success' />
 				</div>
-				<div className='border border-base-300 bg-base-200 p-6 px-8 rounded-sm'>
+				<div className='border border-base-300 bg-base-200 p-6 px-8 rounded-sm w-full lg:w-fit'>
 					<code className='text-secondary uppercase font-black text-xl tracking-widest flex items-center gap-2'>
 						zestaw 2in1{' '}
 						<div className='badge badge-neutral text-xs uppercase font-sans font-semibold tracking-tighter'>
 							Nowość
 						</div>
 					</code>
-					<p className='text-sm'>
-						Kursy kierunkowe - <b>MS Office</b> lub <b>Python + algorytmika</b>.
-					</p>
+					<div className='text-sm h-10'>
+						<span className='inline-block'>
+							Kursy kierunkowe - <b>MS Office</b>
+						</span>{' '}
+						<span className='inline-block'>
+							lub <b>Python + algorytmika</b>.
+						</span>
+					</div>
 					<div className='divider' />
 					<span className='text-4xl font-bold'>120</span>
-					<span className='text-2xl'>,00zł</span>
-					<span className='text-xs ml-4'>/ jednorazowa płatność</span>
+					<span className='text-2xl'>,00 PLN</span>
+					<div className='text-xs ml-4'>/ jednorazowa płatność</div>
 					<div className='divider' />
-					<p className='text-sm mb-2'>
-						Zawiera jeden z dostępnych pakietów (2 kursy):
-					</p>
+					<div className='text-sm mb-2 h-10'>
+						<span className='inline-block'>Zawiera jeden z dostępnych</span>{' '}
+						<span className='inline-block'>pakietów (2 kursy):</span>
+					</div>
 					<div className='flex items-center gap-1 [&>*:nth-child(3n+2)]:-mr-8'>
 						<img className='h-14' src={kursPython} alt='kursPython' />
 						<span></span>
@@ -81,6 +97,9 @@ const Kursy: React.FC = () => {
 						<span></span>
 						<img className='h-14' src={kursAccess} alt='kursAccess' />
 					</div>
+					<button className='block btn-success hover:btn-neutral btn btn-sm w-24 ml-auto mt-16'>
+						Kup
+					</button>
 				</div>
 			</div>
 		</>
