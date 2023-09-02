05dfe5aed41d4add6890ac947fc00774b81e2b14
Folderas <82723806+marcinwolder@users.noreply.github.com>
2023-09-02 17:38:33 +0200

    auth modal switch working

--- src/components/AuthComp.tsx
+++ src/components/AuthComp.tsx
@@ -1,14 +1,47 @@
 import { modals } from '@mantine/modals';
 
 const AuthComp = () => {
-	const openModal = () =>
+	const openSignUpModal = () =>
 		modals.open({
-			title: <p>Zaloguj się</p>,
+			modalId: 'signUpModal',
+			title: (
+				<div className='flex flex-col gap-1'>
+					<div
+						className='text-base-content underline underline-offset-2 hover:no-underline hover:text-info hover:cursor-pointer'
+						onClick={() => {
+							modals.close('signUpModal');
+							openSignInModal();
+						}}>
+						Zaloguj się
+					</div>
+					<p className='font-bold uppercase font-mono'>/ Zarejestruj się</p>
+				</div>
+			),
+			children: <p className='text-red-400'>TEST</p>,
+		});
+	const openSignInModal = () =>
+		modals.open({
+			modalId: 'signInModal',
+			title: (
+				<div className='flex flex-col gap-1'>
+					<p className='font-bold uppercase font-mono'>Zaloguj się</p>
+					<div
+						className='text-base-content underline underline-offset-2 hover:no-underline hover:text-info hover:cursor-pointer'
+						onClick={() => {
+							modals.close('signInModal');
+							openSignUpModal();
+						}}>
+						/ Zarejestruj się
+					</div>
+				</div>
+			),
 			children: <p className='text-red-400'>TEST</p>,
 		});
 
 	return (
-		<div className='btn text-xs px-2 gap-0 w-min md:w-fit' onClick={openModal}>
+		<div
+			className='btn text-xs px-2 gap-0 w-min md:w-fit'
+			onClick={openSignInModal}>
 			<div className='m-2'>
 				<span className='inline-block'>ZALOGUJ</span>{' '}
 				<span className='hidden md:inline'>/</span>{' '}
