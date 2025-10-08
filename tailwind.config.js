3b65a2ed8124325888e7a815acdaa93347ec2728
marcinwolder <marcinwolder7@gmail.com>
2025-10-09 00:01:49 +0200

    fixed DaisyUI in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,29 @@
-/** @type {import('tailwindcss').Config} */
-
+import { createRequire } from "module";
 import daisyuiPlugin from "daisyui";
 import opentypePlugin from "tailwindcss-opentype";
-import tailwindTextShadowPlugin from "@designbycode/tailwindcss-text-shadow";
 import { themeNames } from "./src/utils/theme.ts";
 
-export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
-export const theme = {
-  extend: {
-    textShadow: {
-      codeBack: "0 0 40px rgba(0,0,0,0.5)",
-    },
-    colors: {
-      stara: "#8c8c8c",
-      2015: "#bfbfbf",
-      2023: "#d5b8ea",
+const require = createRequire(import.meta.url);
+const tailwindTextShadowPlugin = require(
+  "@designbycode/tailwindcss-text-shadow",
+);
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      textShadow: {
+        codeBack: "0 0 40px rgba(0,0,0,0.5)",
+      },
+      colors: {
+        stara: "#8c8c8c",
+        2015: "#bfbfbf",
+        2023: "#d5b8ea",
+      },
     },
   },
-};
-export const plugins = [
-  opentypePlugin,
-  daisyuiPlugin,
-  tailwindTextShadowPlugin,
-];
-export const daisyui = {
-  themes: themeNames,
+  plugins: [opentypePlugin, daisyuiPlugin, tailwindTextShadowPlugin],
+  daisyui: {
+    themes: themeNames,
+  },
 };
